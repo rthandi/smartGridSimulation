723eb88afa0b102fab59002f4f35628b8ccf3da5
Robbie Thandi <robbie.thandi@gmail.com>
2021-06-25 15:56:25 +0200

    added some logic to main loop to select whether importer or exporter.

--- testSimul.py
+++ testSimul.py
@@ -1,3 +1,5 @@
+import random
+
 retailPrice = 10
 feedInTariff = 2
 
@@ -17,4 +19,21 @@ erin = User("Erin")
 fred = User("Fred")
 gary = User("Gary")
 
+users = [alice, bob, charlie, dean, erin, fred, gary]
+importers = None
+exporters = None
+
 for i in range(4):
+    importers = []
+    exporters = []
+    for j in users:
+        # choose whether importer or exporter
+        # More realistic to not be 50/50 but this can be changed later
+        if random.randint(1,2) == 1:
+            j.imported = random.randint(2, 100)
+            importers.append(j)
+        else:
+            # Probably more realistic to expect exports are lower than imports but will leave it like this for now
+            j.exported = random.randint(2, 100)
+            exporters.append(j)
+

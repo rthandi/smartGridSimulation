4dcff42e0151a30ecc50bcd47dfcad5df87f29d9
Robbie Thandi <robbie.thandi@gmail.com>
2021-07-14 19:14:00 +0100

    small changes whilst fixing issues

--- simulation.py
+++ simulation.py
@@ -7,19 +7,25 @@ from shapely.geometry import LineString
 # constants
 RETAIL_PRICE = 10
 FEED_IN_TARIFF = 2
-TRADING_PERIODS = 5
+TRADING_PERIODS = 1
 
 
 # Notes: Make it easy to compare to using the normal markets
 # Look at homomorphic encrypt libs
 
 class User:
-    def __init__(self, name):
+    def __init__(self, name = None):
         self.name = name
         self.imported = 0
         self.exported = 0
         self.realTradeAmount = 0
         self.bill = 0
+        self.bid = 0
+
+    def reset(self):
+        self.imported = 0
+        self.exported = 0
+        self.realTradeAmount = 0
         self.bid = 0
 
 
@@ -71,10 +77,10 @@ def doubleAuction(auctionImporters, auctionExporters):
     sellList.sort(key=lambda item: item['price'])
     buyList.sort(key=lambda item: item['price'], reverse=True)
 
-    cumBuy = np.cumsum(list(d['amount'] for d in buyList))
-    cumSell = np.cumsum(list(d['amount'] for d in sellList))
-    sellPriceColumn = list(d['price'] for d in sellList)
-    buyPriceColumn = list(d['price'] for d in buyList)
+    cumBuy = np.array(np.cumsum(list(d['amount'] for d in buyList)))
+    cumSell = np.array(np.cumsum(list(d['amount'] for d in sellList)))
+    sellPriceColumn = np.array(list(d['price'] for d in sellList))
+    buyPriceColumn = np.array(list(d['price'] for d in buyList))
 
     # plt.step(cumBuy, buyPriceColumn)
     # plt.step(cumSell, sellPriceColumn)
@@ -86,7 +92,7 @@ def doubleAuction(auctionImporters, auctionExporters):
     line_1 = LineString(np.column_stack((cumBuy, buyPriceColumn)))
     line_2 = LineString(np.column_stack((cumSell, sellPriceColumn)))
     intersection = line_1.intersection(line_2)
-    if (intersection):
+    if intersection:
         plt.plot(*intersection.xy, 'ro')
         plt.show()
         return intersection.y
@@ -99,12 +105,12 @@ def auction_winners(users_arg, importers_arg, exporters_arg):
     trading_price = doubleAuction(importers_arg, exporters_arg)
     traders = set()
     if trading_price:
-        trading_price = round(trading_price, 2)
+        trading_price = trading_price
         print("Trading price for this trading period is: " + str(trading_price))
         for current_user in users_arg:
             # for each user we see if their bid is in the correct range to trade
             if (current_user in importers_arg and current_user.bid >= trading_price
-                    or current_user in exporters_arg and current_user.bid >= trading_price):
+                    or current_user in exporters_arg and current_user.bid <= trading_price):
                 traders.add(current_user)
     else:
         print("No intersection of supply demand curves - no trading will occur this period")
@@ -120,10 +126,13 @@ def execute_trades(traders, non_traders, importers_arg, trading_price):
             nonTrader.bill += nonTrader.imported * RETAIL_PRICE
         else:
             nonTrader.bill -= nonTrader.exported * FEED_IN_TARIFF
+        nonTrader.reset()
 
+    volumeTraded = 0
     for export_trader in (traders - importers_arg):
         real_amount = export_trader.realTradeAmount
         committed_amount = export_trader.exported
+        volumeTraded += committed_amount
         if committed_amount >= real_amount:
             # they sell the committed amount or more - excess sold for FIT
             tariff = FEED_IN_TARIFF
@@ -131,10 +140,12 @@ def execute_trades(traders, non_traders, importers_arg, trading_price):
             # they sell less than the committed amount - buy from retail
             tariff = RETAIL_PRICE
         export_trader.bill -= committed_amount * trading_price - (real_amount - committed_amount) * tariff
+        export_trader.reset()
 
     for import_trader in (traders.intersection(importers_arg)):
         real_amount = import_trader.realTradeAmount
-        committed_amount = import_trader.exported
+        committed_amount = import_trader.imported
+        volumeTraded -= committed_amount
         if committed_amount <= real_amount:
             # they use the committed amount or more - extra purchased from retail
             tariff = RETAIL_PRICE
@@ -142,25 +153,39 @@ def execute_trades(traders, non_traders, importers_arg, trading_price):
             # they use less than the committed amount - excess sold for FIT
             tariff = FEED_IN_TARIFF
         import_trader.bill += committed_amount * trading_price + (real_amount - committed_amount) * tariff
+        import_trader.reset()
 
+    print("volume Traded:         " + str(volumeTraded))
     return traders | non_traders
 
 
 def simulate(trading_periods):
-    alice = User("Alice")
-    bob = User("Bob")
-    charlie = User("Charlie")
-    dean = User("Dean")
-    erin = User("Erin")
-    fred = User("Fred")
-    gary = User("Gary")
-    harry = User("Harry")
-    imogen = User("Imogen")
-    john = User("John")
+    # alice = User("Alice")
+    # bob = User("Bob")
+    # charlie = User("Charlie")
+    # dean = User("Dean")
+    # erin = User("Erin")
+    # fred = User("Fred")
+    # gary = User("Gary")
+    # harry = User("Harry")
+    # imogen = User("Imogen")
+    # john = User("John")
+    #
+    # users = {alice, bob, charlie, dean, erin, fred, gary, harry, imogen, john}
+    # importers = {alice, bob, charlie, dean, erin, fred}
+    # exporters = {gary, harry, imogen, john}
 
-    users = {alice, bob, charlie, dean, erin, fred, gary, harry, imogen, john}
-    importers = {alice, bob, charlie, dean, erin, fred}
-    exporters = {gary, harry, imogen, john}
+    users = set()
+    importers = set()
+    exporters = set()
+    for i in range(1000):
+        users.add(User(str(i)))
+
+    for user in users:
+        if random.randint(1, 3) == 1:
+            exporters.add(user)
+        else:
+            importers.add(user)
 
     for currentTradingPeriod in range(trading_periods):
         # select a bid price and export/import amount for each user
@@ -183,9 +208,9 @@ def simulate(trading_periods):
         users = execute_trades(traders, non_traders, importers, trading_price)
 
         # for testing only
-        for current_user in users:
-            print("name: " + current_user.name + " imported: " + str(current_user.imported) + " exported: " +
-                  str(current_user.exported) + " current bill: " + str(current_user.bill))
+        # for current_user in users:
+        #     print("name: " + current_user.name + " imported: " + str(current_user.imported) + " exported: " +
+        #           str(current_user.exported) + " current bill: " + str(current_user.bill))
 
     return users
 
